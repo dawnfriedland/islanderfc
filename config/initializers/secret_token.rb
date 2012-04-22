329b326a836e3dfe9a87d9581afcd8fc5ef41b99
dawnfriedland <dawn@barklouder.com>
2012-04-22 07:48:07 -0700

    initial commit with spree and activeadmin working

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Islanderfc::Application.config.secret_token = 'f1b8082d567a0ef347388a838fdd090e5d31c7f63523b50d04722d33eb43f8957f1bd8dedf5e9c6455eb6330c2cb9f9464652bd3b2825043e3acf16950433d6d'
